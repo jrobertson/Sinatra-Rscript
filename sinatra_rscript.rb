e41d591c948e9b9938472fa5dad2a1f79ac0d05b
James Robertson <james@r0bertson.co.uk>
2009-10-20 00:24:01 +0100

    server.xml support added

--- sinatra_rscript.rb
+++ sinatra_rscript.rb
@@ -358,5 +358,7 @@ post '/*' do
 end
 
 # boot script
+doc = Document.new(File.open('server.xml','r').read)
+server_name = XPath.first(doc.root, 'summary/name/text()').to_s
 url = url_base + 'r.rsf'
-run(url, '//job:bootstrap')
+run(url, '//job:bootstrap', server_name)
